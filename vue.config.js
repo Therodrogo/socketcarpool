4660f1c82432e80e3fc0f40a7c8bb0151b816331
Therodrogo <52356618+Therodrogo@users.noreply.github.com>
2023-05-30 22:39:57 -0400

    sad

--- vue.config.js
+++ vue.config.js
@@ -2,9 +2,8 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
-
 module.exports = {
   devServer: {
-    https: false
+    https: true
   }
 };
